5535ecba46b7a2c1245d0e22d3b80c88d612f091
pratima-dev25 <krpratima655@gmail.com>
2026-06-11 12:22:06 +0530

    features improvement

--- Task-1-todo-app/src/components/TodoList.jsx
+++ Task-1-todo-app/src/components/TodoList.jsx
@@ -7,7 +7,7 @@ export default function TodoList({todos, deleteTodo, doneTask, darkMode}){
     
     return(
 
-        <div className="flex justify-around items-center flex-wrap gap-2 w-full m-5">
+        <div className="flex justify-around items-center flex-wrap gap-2 w-ful max-w-4xl m-5">
 
             <h1 className={ darkMode ? "font-bold text-2xl text-center mt-10 text-white"
             :"font-bold text-2xl text-center mt-10 "}>
