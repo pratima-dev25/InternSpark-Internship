c972ffb1d50139b9d8285412237ca1090046d7b9
pratima-dev25 <krpratima655@gmail.com>
2026-06-11 12:22:39 +0530

    features improvement

--- Task-1-todo-app/src/components/TodoList.jsx
+++ Task-1-todo-app/src/components/TodoList.jsx
@@ -7,7 +7,7 @@ export default function TodoList({todos, deleteTodo, doneTask, darkMode}){
     
     return(
 
-        <div className="flex justify-around items-center flex-wrap gap-2 w-ful max-w-4xl m-5">
+        <div className="flex justify-around items-center flex-wrap gap-2 w-ful max-w-4xl max-auto m-5">
 
             <h1 className={ darkMode ? "font-bold text-2xl text-center mt-10 text-white"
             :"font-bold text-2xl text-center mt-10 "}>
@@ -21,7 +21,7 @@ export default function TodoList({todos, deleteTodo, doneTask, darkMode}){
                 <ul>
                     
                     {todos.map((todo) => (
-                        <div className="flex justify-between items-center w-full bg-gray-300 shadow-xl/20 rounded-2xl p-3 m-10 break-all whitespace-normal ">
+                        <div className="flex justify-between items-center gap-2 w-full bg-gray-300 shadow-xl/20 rounded-2xl p-3 m-10 break-all whitespace-normal ">
 
                             <li className=" flex-1 md:text-xl list-disc list-inside h-10 w-full" key={todo.id}>
                                 <span style={todo.isDone ? {textDecorationLine: "line-through"} : {}}> {todo.task} </span>
